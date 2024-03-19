22b64d45b9329b5cf95879bbe6a9ec726be40689
AshutoshDM1 <downlodemaster1@gmail.com>
2024-03-19 12:40:40 +0530

    bg_ change_3

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Weather from "./Component/Weather";
 import "./Component/CSS Files/app.css";
 
 function App() {
-
+let bg_Image_url = "https://img.freepik.com/free-vector/elegant-blurred-background_1393-113.jpg?size=626&ext=jpg&ga=GA1.1.735520172.1710633600&semt=ais"
   return (
     <>
-      <div className="page" style={{ backgroundImage: `url("./src/assets/sunset_BG.jpg")` }}>
+      <div className="page" style={{ backgroundImage: `url("${bg_Image_url}")` }}>
         <Header />
         <Weather></Weather>
         <div className="like"></div>
